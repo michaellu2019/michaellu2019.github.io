a3b16a2f4445058654f6ceece24aabe8ccb6b0ed
Michael Lu <mlu0708@mit.edu>
2024-08-24 14:38:21 -0700

    No more loading page...:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { React, useEffect, useState } from 'react';
+import { React } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './styles/styles.scss';
 
 import Navbar from './components/Navbar.js';
 import Footer from './components/Footer.js';
-import Loading from './pages/Loading.js';
 import Cover from './pages/Cover.js';
 import Projects from './pages/Projects.js';
 import Project from './pages/Project.js';
 import Work from './pages/Work.js';
 
 function App() {
-  const [ loading, setLoading ] = useState(true);
-  const handleLoading = () => {
-    setLoading(false);
-  }
+  // const [ loading, setLoading ] = useState(true);
+  // const handleLoading = () => {
+  //   setLoading(false);
+  // }
 
-  useEffect(() => {
-    window.addEventListener("load", handleLoading);
-    return () => window.removeEventListener("load", handleLoading);
-  }, [])
+  // useEffect(() => {
+  //   window.addEventListener("load", handleLoading);
+  //   return () => window.removeEventListener("load", handleLoading);
+  // }, [])
 
   function scrollTo(section) {
     if (!document.querySelector(`.${section}-section-container`)) return;
@@ -31,26 +30,22 @@ function App() {
     });
   }
 
-  return loading ? 
-  (<Loading />) :
-  (
-    <div className="wrapper">
-      <main className="primary">
-        <Routes>
-          <Route path="/" Component={() => <div>
-            <Cover scrollTo={scrollTo} />
-            <Projects />
-            <Work />
-          </div>} />
-          <Route path="/:id" Component={() => <div>
-            <Project />
-          </div>} />
-        </Routes>
-      </main>
-      <Navbar scrollTo={scrollTo} />
-      <Footer />
-    </div>
-  )
+  return <div className="wrapper">
+    <main className="primary">
+      <Routes>
+        <Route path="/" Component={() => <div>
+          <Cover scrollTo={scrollTo} />
+          <Projects />
+          <Work />
+        </div>} />
+        <Route path="/:id" Component={() => <div>
+          <Project />
+        </div>} />
+      </Routes>
+    </main>
+    <Navbar scrollTo={scrollTo} />
+    <Footer />
+  </div>
 }
 
 export default App;
