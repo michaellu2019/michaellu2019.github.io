b313280287bdbe50d120fa4a5ad005240b0114c7
Michael Lu <mlu0708@mit.edu>
2025-07-29 16:41:03 -0700

    Updated bio.:

--- src/pages/Cover.js
+++ src/pages/Cover.js
@@ -12,10 +12,14 @@ function Cover(props) {
           <img alt="" src={dummy} />
         </div>
         <div className="cover-text">
-          I make hardware ⚙️ go beep boop bop 🤖. I studied mechanical engineering along with electrical engineering and computer science at MIT.
+          {/* I make hardware ⚙️ go beep boop bop 🤖. I studied mechanical engineering along with electrical engineering and computer science at MIT.
           <br /> 
           <br />
-          I've worked as a software engineering intern at Amazon, electrical engineering intern at Milwaukee Tool, mechatronics intern at SpaceX, and am currently working on robots for the moon at NASA JPL.
+          I've worked as a software engineering intern at Amazon, electrical engineering intern at Milwaukee Tool, mechatronics intern at SpaceX, and am currently working on robots for the moon at NASA JPL. */}
+          Every since I was born, I knew that I wanted to be that one dude that other dudes would look up to and say to other dudes, "Dude, I want to be that dude."
+          <br />
+          <br />
+          Today, I have become an exceptionally mediocre engineer and experienced amateur programmer after trudging through the MechE and EECS curriculum at MIT for a few years.
         </div>
       </div>
     </article>
